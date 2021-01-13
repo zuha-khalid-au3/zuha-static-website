0678f726a6e58d8636dbe1e9a15b1376db6987be
zuha-khalid-au3 <zuhakhalid@gmail.com>
2021-01-13 15:48:30 +0530

    Build script

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello <code>src/App.js</code> and save to reload.
         </p>
+        <h1>hello</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
